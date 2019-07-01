f66a1012fa1dc1698d229b5f60e52492e200313c
ArtemNagaev <31846535+ArtemNagaev@users.noreply.github.com>
2019-07-01 19:57:20 +0500

    Update 1.4
    Added:
    -CursorChangerScript
    -Cursor texture

---
+++ Assets/Scripts/CursorChangerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorChangerScript : MonoBehaviour
+{
+    //texture of cursor
+    public Texture2D cursorImage;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //setting cursor
+        Cursor.SetCursor(cursorImage, new Vector2(cursorImage.width / 2, cursorImage.width / 2), CursorMode.ForceSoftware);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
