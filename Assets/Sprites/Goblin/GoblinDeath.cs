9740c09b1a30113deeb1533b142c9e50ca058e8f
ArtemNagaev <31846535+ArtemNagaev@users.noreply.github.com>
2019-07-02 14:30:01 +0500

    Update 1.6
    Добавлена тестовая анимация смерти гоблина

---
+++ Assets/Sprites/Goblin/GoblinDeath.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinDeath : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Vector2 startPoint;
+
+    public Vector2 endPoint;
+
+    public Vector2 lerpPoint;
+
+    public Vector2 direction;
+
+    private GameObject go;
+
+    private SpriteRenderer sr;
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+
+        startPoint = transform.position;
+        endPoint = startPoint + direction * 2;
+    }
+
+    // Update is called once per frame
+
+    private float count = 0.0f;
+
+    
+
+    void Update()
+    {
+        if (direction.x < 0)
+        {
+            sr.flipX = false;
+        }
+        if (direction.x > 0)
+        {
+            sr.flipX = true;
+        }
+
+
+        if (count < 1.0f)
+        {
+            count += 2.0f * Time.deltaTime;
+
+            //Vector3 m1 = Vector3.Lerp(startPoint, endPoint, count);
+            //Vector3 m2 = Vector3.Lerp(lerpPoint, endPoint, count);
+            transform.position = Vector3.Lerp(startPoint, endPoint, count);
+        }
+    }
+}
