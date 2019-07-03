5258638945ee91f557745e20c4732d960640b06c
ArtemNagaev <31846535+ArtemNagaev@users.noreply.github.com>
2019-07-03 23:04:02 +0500

    Update 1.14
    Эффект получения урона противником и персонажем

---
+++ Assets/Scripts/BlinkingScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlinkingScript : MonoBehaviour
+{
+
+    private SpriteRenderer sr;
+
+    public bool isBlinking;
+
+    private bool isBlink;
+
+    public Material blinkMaterial;
+
+    private Material startMaterial;
+
+    public float blinkingDuration = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+        startMaterial = sr.material;
+
+        StartCoroutine("Blink");
+    }
+
+    IEnumerator Blink()
+    {
+        while (true)
+        {
+            if (isBlinking)
+            {
+                yield return new WaitForSeconds(blinkingDuration);
+                isBlink = !isBlink;
+                if (isBlink)
+                {
+                    sr.material = blinkMaterial;
+
+                }
+                else
+                {
+                    sr.material = startMaterial;
+                }
+            }
+            yield return null;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
